57cb50d876115f2fc8efe1bbbbeb683953df92b8
sylvester <chepapesylvester@gmail.com>
2020-10-27 02:18:57 +0200

    toggler

--- js/product.js
+++ js/product.js
@@ -37,3 +37,55 @@ window.addEventListener("scroll",function(){
     });
   }, false);
 })();
+
+
+//form toggler
+function _id(name){
+  return document.getElementById(name);
+}
+function _class(name){
+  return document.getElementsByClassName(name);
+}
+_class("toggle-password")[0].addEventListener("click",function(){
+  _class("toggle-password")[0].classList.toggle("active");
+  if(_id("password").getAttribute("type") == "password"){
+    _id("password").setAttribute("type","text");
+  } else {
+    _id("password").setAttribute("type","password");
+  }
+});
+
+_id("password").addEventListener("focus",function(){
+  _class("password-policies")[0].classList.add("active");
+});
+_id("password").addEventListener("blur",function(){
+  _class("password-policies")[0].classList.remove("active");
+});
+
+_id("password").addEventListener("keyup",function(){
+  let password = _id("password").value;
+  
+  if(/[A-Z]/.test(password)){
+    _class("policy-uppercase")[0].classList.add("active");
+  } else {
+    _class("policy-uppercase")[0].classList.remove("active");
+  }
+  
+  if(/[0-9]/.test(password)){
+    _class("policy-number")[0].classList.add("active");
+  } else {
+    _class("policy-number")[0].classList.remove("active");
+  }
+  
+  if(/[^A-Za-z0-9]/.test(password)){
+    _class("policy-special")[0].classList.add("active");
+  } else {
+    _class("policy-special")[0].classList.remove("active");
+  }
+  
+  if(test(password).length > 7){
+    _class("policy-length")[0].classList.add("active");
+  } else {
+    _class("policy-length")[0].classList.remove("active");
+  }
+});
